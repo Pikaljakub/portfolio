5e0b1e679275dd580b91fb1149105fb39d98985a
Pikaljakub <165057334+Pikaljakub@users.noreply.github.com>
2024-04-02 10:58:31 +0200

    Add files via upload

---
+++ PRG22.py
@@ -0,0 +1,13 @@
+'''
+Hodnota vašeho vkladu po 10 letech při 5% úrokové sazbě je 1629 Kč.
+'''
+vklad = 1000
+roky = 10
+urok = 0.05
+
+hodnota = vklad
+for rok in range(0, roky):
+    hodnota += hodnota * urok 
+    
+
+print(f"Hodnota vašeho vkladu po {roky} letech při {urok * 100:.0f}% úrokové sazbě je {hodnota:.0f} Kč.")
